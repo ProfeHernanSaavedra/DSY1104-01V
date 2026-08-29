006fbdfb8e0c4d07e785905260fa4971d29c86bc
profehernanSaavedra <hern.saavedra@profesor.duoc.cl>
2026-08-29 09:52:06 -0400

    javaScript personalizadosy agregar productos

--- js/app.js
+++ js/app.js
@@ -14,3 +14,47 @@ botonBienvenida.addEventListener("click",function() {
     botonBienvenida.textContent = "Bienvenida mostrada"
 });
 
+const botonPromocion = document.getElementById("boton-promocion");
+const promocion =document.getElementById("promocion");
+
+//botonPromocion.addEventListener("click",function(){
+//    promocion.classList.remove("d-none");
+//});
+
+botonPromocion.addEventListener("click",function(){
+    promocion.classList.toggle("d-none");
+
+    if(promocion.classList.contains("d-none")){
+        botonPromocion.textContent="Mostrar Promoción";
+    }
+    else{
+        botonPromocion.textContent="Ocultar Promoción";
+    }
+
+});
+
+const bontonesAgregar =document.querySelectorAll(".boton-agregar");
+const contador = document.getElementById("contador-productos");
+const ultimoProducto = document.getElementById("ultimo-producto");
+
+let cantidadProductos = 0;
+
+bontonesAgregar.forEach(function(boton){
+    boton.addEventListener("click",function(){
+        const nombreProducto =boton.dataset.producto;
+        //console.log("Producto Seleccionado: "+ nombreProducto);
+
+        cantidadProductos=cantidadProductos +1
+
+        contador.textContent=cantidadProductos;
+
+        ultimoProducto.textContent =nombreProducto+" fue agregado correctamente";
+
+    });
+});
+
+
+
+
+
+
